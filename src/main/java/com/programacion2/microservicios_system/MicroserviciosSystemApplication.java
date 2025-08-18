7c25b07b204a545254167c01850bdffc0093e2b9
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2025-08-18 18:48:31 -0300

    First commit

---
+++ src/main/java/com/programacion2/microservicios_system/MicroserviciosSystemApplication.java
@@ -0,0 +1,13 @@
+package com.programacion2.microservicios_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programacion2/microservicios_system/MicroserviciosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programacion2.microservicios_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
