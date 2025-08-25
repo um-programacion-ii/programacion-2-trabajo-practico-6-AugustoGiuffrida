05481088c499446757c5e15fad80341a87cbf03d
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2025-08-25 13:47:55 -0300

    fix project structure

---
+++ business-service/src/main/java/com/business_service/business_service/BusinessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.business_service.business_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessServiceApplication.class, args);
+	}
+
+}
